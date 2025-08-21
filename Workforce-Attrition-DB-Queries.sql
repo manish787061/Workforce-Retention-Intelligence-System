3d01f04c10bf528161c0bb280413efc4c07b2f8e
manish787061 <ankuraman2006@gmail.com>
2025-08-21 21:30:31 +0530

    Add files via upload

---
+++ Workforce-Attrition-DB-Queries.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS Employee_Dataset; 
+CREATE TABLE Employee_Dataset (
+    Participant TEXT,
+    Age INT,
+    Job_Satisfaction TEXT,
+    Business_Travel TEXT,
+    Department TEXT,
+    Distance_From_Home_Miles INT,
+    Education_Field TEXT,
+    Gender TEXT,
+    Job_Role TEXT,
+    Marital_Status TEXT,
+    Monthly_Income INT,
+    Over_Time TEXT,
+    Percent_Salary_Hike INT,
+    Performance_Rating INT,
+    Relationship_Satisfaction INT,
+    Total_Working_Years INT,
+    Years_At_Company INT,
+    Years_In_Current_Role INT,
+    Years_Since_Last_Promotion INT,
+    Years_With_Current_Manager INT
+);
+SELECT*FROM Employee_Dataset;
